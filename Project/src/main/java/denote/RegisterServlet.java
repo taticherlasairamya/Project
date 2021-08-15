5e1853eb38cc420a12b39554d829f09ab93dddea
taticherlasairamya <87537778+taticherlasairamya@users.noreply.github.com>
2021-08-15 10:05:35 +0530

    Update RegisterServlet.java

--- Project/src/main/java/denote/RegisterServlet.java
+++ Project/src/main/java/denote/RegisterServlet.java
@@ -1,54 +1,35 @@
 package denote;
-
 import java.io.IOException;
-
-
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+ 
 
-
-
-/**
- * @email Ramesh Fadatare
- */
-
-@WebServlet("/Register")
-public class RegisterServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private RegisterDao registerDao;
-
-    public void init() {
-        registerDao = new RegisterDao();
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-
-        String name = request.getParameter("name");
-      
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        String phoneno = request.getParameter("phoneno");
-       
-
-        Register register = new Register();
-        register.setname(name);
-        
-        register.setemail(email);
-        register.setpassword(password);
-        register.setphoneno(phoneno);
-       
-
-        try {
-            registerDao.registerUser(register);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        response.sendRedirect("RegisterSuccess.jsp");
-    }
+ 
+public class RegisterServlet extends HttpServlet 
+{ 
+     public RegisterServlet() 
+     {
+     }
+     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+     { 
+         Register register = new Register();       
+         register.setName(request.getParameter("name"));
+         register.setPassword(request.getParameter("password")); 
+         register.setEmail(request.getParameter("email"));              
+         register.setPhoneno(request.getParameter("phoneno"));         
+         RegisterDao registerDao = new RegisterDao();
+         String userRegistered = registerDao.registerUser(register);         
+         if(userRegistered.equals("SUCCESS"))  
+         {
+            request.getRequestDispatcher("RegisterSuccess.jsp").forward(request, response);
+         }
+         else  
+         {
+            request.setAttribute("errMessage", userRegistered);
+            request.getRequestDispatcher("/RegisterError.jsp").forward(request, response);
+         }
+     }
+    
 }
