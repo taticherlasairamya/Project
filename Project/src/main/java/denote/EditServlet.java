2cafe4b2e6fe5a5e43700de6b7adb7cceab53354
taticherlasairamya <87537778+taticherlasairamya@users.noreply.github.com>
2021-08-15 10:18:00 +0530

    Add files via upload

---
+++ Project/src/main/java/denote/EditServlet.java
@@ -0,0 +1,37 @@
+package denote;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+
+ 
+public class EditServlet extends HttpServlet 
+{ 
+     public EditServlet() 
+     {
+     }
+     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+     { 
+         Register register = new Register();       
+         register.setEmail(request.getParameter("email"));
+         
+         register.setName(request.getParameter("name"));              
+         register.setPhoneno(request.getParameter("phoneno"));    
+         register.setGender(request.getParameter("gender")); 
+         register.setDob(request.getParameter("dob")); 
+         RegisterDao registerDao = new RegisterDao();
+         String userRegistered = registerDao.editUser(register);         
+         if(userRegistered.equals("SUCCESS"))  
+         {
+            request.getRequestDispatcher("RegisterSuccess.jsp").forward(request, response);
+         }
+         else  
+         {
+            request.setAttribute("errMessage", userRegistered);
+            request.getRequestDispatcher("/RegisterError.jsp").forward(request, response);
+         }
+     }
+    
+}
