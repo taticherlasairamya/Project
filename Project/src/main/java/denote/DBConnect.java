7f9afee2cd630cfde6ee53fbbdb756b7cfc4fcb9
taticherlasairamya <87537778+taticherlasairamya@users.noreply.github.com>
2021-08-16 21:25:59 +0530

    Update DBConnect.java

--- Project/src/main/java/denote/DBConnect.java
+++ Project/src/main/java/denote/DBConnect.java
@@ -2,32 +2,32 @@ package denote;
 import java.sql.Connection;
 import java.sql.DriverManager;
  
-public class DBConnect {
- public static Connection createConnection()
- {
-     Connection con = null;
-     String url = "jdbc:mysql://localhost:3306/denote"; //MySQL URL followed by the database name
-     String username = "root"; //MySQL username
-     String password = "ramya"; //MySQL password
-     System.out.println("In DBConnection.java class ");
-      
-     try
-     {
-         try
-         {
-            Class.forName("com.mysql.cj.jdbc.Driver"); //loading MySQL drivers. This differs for database servers 
-         } 
-         catch (ClassNotFoundException e)
-         {
-            e.printStackTrace();
-         }       
-         con = DriverManager.getConnection(url, username, password); //attempting to connect to MySQL database
-         System.out.println("Printing connection object "+con);
-     } 
-     catch (Exception e) 
-     {
-        e.printStackTrace();
-     }   
-     return con; 
- }
+public class DBConnect
+{
+	public static Connection createConnection()
+	{
+		Connection con = null;
+		String url = "jdbc:mysql://localhost:3306/denote"; 
+		String username = "root"; 
+		String password = "ramya";
+		System.out.println("In DBConnection.java class ");
+      	try
+      	{
+      		try
+      		{
+      			Class.forName("com.mysql.cj.jdbc.Driver");  
+      		} 
+      		catch (ClassNotFoundException e)
+      		{
+      			e.printStackTrace();
+        	}       
+      	con = DriverManager.getConnection(url, username, password); 
+        System.out.println("Printing connection object "+con);
+      	} 
+      	catch (Exception e) 
+      	{
+      		e.printStackTrace();
+      	}   
+      	return con; 
+	}
 }
